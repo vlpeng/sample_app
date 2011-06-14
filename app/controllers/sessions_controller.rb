e9ed8546376bae4c6d2960f7bc00f28d72886b23
Josh Pen <vlpeng@gmail.com>
2011-06-14 16:43:17 +0800

    Completed signin/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,8 +15,11 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_to user
   end
- end
+end
+
   def destroy
+    sign_out
+    redirect_to root_path
   end
 
 end
