f2d85913bc945d1f9f3b933e1f72927125f8d645
Josh Pen <vlpeng@gmail.com>
2011-06-14 11:16:21 +0800

    Added a Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+   @title = "Sign in"
+  end
+
+  def create
+   user = User.authenticate(params[:session][:email],
+                            params[:session][:password])
+  if user.nil?
+    flash.now[:error] = "Invaild email/password combination."
+
+
+   render 'new'
+  else
+  end
+ end
+  def destroy
+  end
+
+end
