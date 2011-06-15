6791d860a6013b2d7a67c999fe47a7551d1abe2d
Josh Pen <vlpeng@gmail.com>
2011-06-15 12:12:25 +0800

    Added correct user def

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+
 
   def show
       @user = User.find(params[:id])
@@ -50,6 +52,11 @@ end
 
     def deny_access
       redirect_to signin_path, :notice => "Please sign in to access this page."
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 
 end
